aa39b3c2c233035a1f8724e64e34ff2046f7fbb8
pallab99 <kitkatpallab@gmail.com>
2023-06-02 11:46:52 +0600

    api and params

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 const app = express();
+const { Schema } = mongoose;
 
+app.use(json());
 const dbConnectionString = process.env.MONGODB_CONNECTION_STRING;
 const port = process.env.PORT;
 mongoose
@@ -14,8 +16,49 @@ mongoose
     console.log(e);
   });
 
+const schema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", schema);
+
 app.get("/", (req, res) => {
   res.send("hello pallab");
+});
+
+app.get("/user/all", async (req, res) => {
+  const Users = await User.find({});
+
+  res.json({
+    success: true,
+    Users,
+  });
+});
+
+app.post("/user/new", async (req, res) => {
+  const { name, email, password } = req.body;
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "registered successfully",
+    newUser,
+  });
+});
+
+app.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  const userByID = await User.findById(id);
+  res.json({
+    success: true,
+    userByID,
+  });
 });
 
 app.listen(port, () => {
